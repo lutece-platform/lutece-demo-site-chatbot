e603ed6c0c67a395fd7e5cc46ea1bedfdabe87ed
Kevin-YUNG <kevyung24@gmail.com>
2018-08-22 20:02:20 +0200

    chatbot demo site changement

--- src/sql/init_db_site.sql
+++ src/sql/init_db_site.sql
@@ -1,7 +1,7 @@
 
 -- override default accounts
 TRUNCATE TABLE core_admin_user;
-INSERT INTO core_admin_user VALUES (1,'admin','Admin','admin','admin@lutece.fr',0,'PLAINTEXT:chatbot','fr',0,0,0,'2020-01-01 00:00:00',null,0,'1980-01-01 00:00:00','all');
+INSERT INTO core_admin_user VALUES (1,'admin','Admin','admin','admin@lutece.fr',0,'PLAINTEXT:chatbot','en',0,0,0,'2020-01-01 00:00:00',null,0,'1980-01-01 00:00:00','all');
 
 
 -- override site properties
@@ -14,7 +14,7 @@ REPLACE INTO core_datastore VALUES ('portal.site.site_property.meta.keywords', '
 REPLACE INTO core_datastore VALUES ('portal.site.site_property.email', 'lutece-dev@paris.fr');
 REPLACE INTO core_datastore VALUES ('portal.site.site_property.noreply_email', 'no-reply@paris.fr');
 REPLACE INTO core_datastore VALUES ('portal.site.site_property.home_url', 'jsp/site/Portal.jsp?page=chatbot');
-REPLACE INTO core_datastore VALUES ('portal.site.site_property.back_images', 'webapp/images/admin/skin/bg_images1.jpg' , 'webapp/images/admin/skin/bg_images2.jpg' , 'webapp/images/admin/skin/bg_images3.jpg' , 'webapp/images/admin/skin/bg_images4.jpg');
+REPLACE INTO core_datastore VALUES ('portal.site.site_property.back_images', '\'images/admin/skin/bg_images1.jpg\',  \'images/admin/skin/bg_images2.jpg\' , \'images/admin/skin/bg_images3.jpg\'');
 
 -- override plugin status
 REPLACE INTO core_datastore VALUES ('core.plugins.status.html.installed' ,'truel' );
